cc13b998b9158e3ccc17a9bddfa229d48072dcdf
maalroba22 <maalroba22@gmail.com>
2023-01-26 17:29:48 -0500

    add to computed example

---
+++ src/components/CompuredExample/ComputedFactura.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <h1>Facturas usando Computed</h1>
+
+    <input type="number" name="" id="" v-model="subTotal">
+   <h2>Total Iva :{{ totalIva }}  ({{ iva }}%)</h2>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import {ref,Ref,computed} from 'vue'
+
+const iva = 19
+let subTotal:Ref<number> =ref(0)
+
+const totalIva =computed(()=> (iva * subTotal.value)/100)
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Post/PostDetails.vue
+++ src/components/Post/PostDetails.vue
@@ -1,46 +1,27 @@
 <template>
     <div class="post">
-        <h3> {{props.title}}</h3>
-        <p>{{props.content}}</p>
-        <input type="text" v-model="message"/>
+        <h3> {{ props.title }}</h3>
+        <p>{{ props.content }}</p>
+        <input type="text" v-model="message" />
         <button @click="handleClickSaludar">Saludar</button>
     </div>
 </template>
 
-<script lang="ts" >
+<script lang="ts" setup>
 
-import { defineComponent,Ref,ref } from 'vue'
-export default defineComponent({
-
-    name: 'PostDetail',
-    props: {
-        title: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            required: false,
-            default: "Post sin contenido"
-
-        }
-
-    },
-
-    emits:['soyhai'],
-    setup(props,{emit}){
-        const handleClickSaludar =()=>{
-           emit("soyhai",message.value)
-        }
-
-        let message:Ref<string> = ref("")
-        return {props,message,handleClickSaludar}
-    }
-
-    
-
-
+import { defineProps, defineEmits, Ref, ref } from 'vue'
+const props = defineProps({
+    title: String,
+    content: String
 })
+const emit =defineEmits(['soyhai'])
+
+const handleClickSaludar = () => {
+    emit("soyhai", message.value)
+}
+
+let message: Ref<string> = ref("")
+
 
 </script>
 
